bb238e4e13069f7982ad3ee0fe149f1644c3a33c
Gwilym Inzani <email@gwilym.dev>
2023-11-08 09:50:14 +0000

    Reply hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{routing::get, Router};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { HelloTemplate { name: "world" } }));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
